d5baea2f392fe9abadd7ac8b4f61699b9126a9b2
Seth Brunwasser <sbrunwasser1998@gmail.com>
2017-11-11 22:48:08 -0700

    routing
    All pages routing finished except for #about

--- __init__.py
+++ __init__.py
@@ -10,5 +10,13 @@ def index():
 def services():
     return render_template("services.html")
 
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/testimonials")
+def testimonials():
+    return render_template("testimonials.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
