f09e1a114632e60416942d300aa545b873801939
Seth Brunwasser <sbrunwasser1998@gmail.com>
2017-11-13 16:50:56 -0700

    Form validation
    Form Validation using post in flask

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -12,24 +12,33 @@ def services():
 
 @app.route("/contact")
 def contact():
-
     return render_template("contact.html")
 
 @app.route("/testimonials")
 def testimonials():
     return render_template("testimonials.html")
 
-@app.route("/submitted")
+@app.route("/submitted", methods=['GET', 'POST'])
 def submitted():
+    error = None
+    if request.method == 'POST':
+        if validation(request.estimateform['name'], request.estimateform['email'],
+                    request.estimateform['phone'], request.estimateform['subject'],
+                    request.estimateform['message']):
+
+            # Send Email to owner
+            sendEmail(request.estimateform['name'], request.estimateform['email'],
+                        request.estimateform['phone'], request.estimateform['subject'],
+                        request.estimateform['message'])
+        else:
+            error = 'Form not valid'
+    return render_template('submitted.html', error=error)
+
+
     return render_template("submitted.html")
-    # Code to contact owner
 
-@app.route("/validation", methods=['GET', 'POST'])
-def validation():
-    # Form validation
-    pass
-
-    return render_template("submitted")
+def sendEmail(name, email, phone, subject, message):
+    submission = False
 
 if __name__ == '__main__':
     app.run(debug=True)
