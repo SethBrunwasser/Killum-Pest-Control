490c4fe832b10ac749a17ba5d3d4ddaef40d4576
Seth Brunwasser <sbrunwasser1998@gmail.com>
2017-11-14 15:20:21 -0700

    Gmail API
    Authentication for Gmail API

--- __init__.py
+++ __init__.py
@@ -1,6 +1,53 @@
 import sqlite3
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, current_app, g
 from contextlib import closing
+from __future__ import print_function
+import httplib2
+import os
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'KillumPestControlTest'
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'gmail-python-quickstart.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
 
 # Configuration
 DATABASE = '/tmp/init.db'
@@ -11,13 +58,15 @@ PASSWORD = 'default'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
-
+'''
 def connect_db():
-    return sqlite3.connect(app.config['DATABASE'])
+    rv = sqlite3.connect(current_app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
 
 def init_db():
     with closing(connect_db()) as db:
-        with app.open_resource('schema.sql', mode='r') as f:
+        with current_app.open_resource('schema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()
 
@@ -46,7 +95,7 @@ def add_entry():
     g.db.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
-
+'''
 
 @app.route("/")
 def index():
@@ -68,23 +117,37 @@ def testimonials():
 def submitted():
     error = None
     if request.method == 'POST':
-        if validation(request.estimateform['name'], request.estimateform['email'],
-                    request.estimateform['phone'], request.estimateform['subject'],
-                    request.estimateform['message']):
+        # Send Email to owner
+        #sendEmail(request.estimateform['name'], request.estimateform['email'],
+        #            request.estimateform['phone'], request.estimateform['subject'],
+        #            request.estimateform['message'])
 
-            # Send Email to owner
-            sendEmail(request.estimateform['name'], request.estimateform['email'],
-                        request.estimateform['phone'], request.estimateform['subject'],
-                        request.estimateform['message'])
-        else:
-            error = 'Form not valid'
+        sendEmail()
     return render_template('submitted.html', error=error)
 
 
     return render_template("submitted.html")
 
-def sendEmail(name, email, phone, subject, message):
-    submission = False
+def sendEmail
+
+def create_message(to, sender, subject, message_text):
+  """Create a message for an email.
+
+  Args:
+    sender: Email address of the sender.
+    to: Email address of the receiver.
+    subject: The subject of the email message.
+    message_text: The text of the email message.
+
+  Returns:
+    An object containing a base64url encoded email object.
+  """
+  message = MIMEText(message_text)
+  message['to'] = to
+  message['from'] = sender
+  message['subject'] = subject
+  return {'raw': base64.urlsafe_b64encode(message.as_string())}
+
 
 if __name__ == '__main__':
     app.run(debug=True)
