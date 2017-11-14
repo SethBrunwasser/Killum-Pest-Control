bbe1ac9c9059b8599451442f10406a1c9ed8037c
Seth Brunwasser <sbrunwasser1998@gmail.com>
2017-11-13 23:08:16 -0700

    db entry
    db add_entry()

--- __init__.py
+++ __init__.py
@@ -31,11 +31,22 @@ def teardown_request(exception):
     if db is not None:
         db.close()
 
+# Show Entries
 @app.route('/')
 def show_entries():
     cur = g.db.execute('select title, text from entries order by id desc')
     entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
     return render_template('show_entries.html', entries = entries)
+
+# Add new entry
+@app.route('/add', methods=['POST'])
+def add_entry():
+    g.db.execute('insert into entries (title, text) value (?, ?)',
+                [request.form['title'], request.form['text']])
+    g.db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
 
 @app.route("/")
 def index():
